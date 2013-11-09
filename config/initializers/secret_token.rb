e0ec13f7f71473d09293d8900d6d192546be1b56
Julius Seizure <ettucrouton@gmail.com>
2013-11-09 10:26:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Betso::Application.config.secret_key_base = '1fd598b0481b9be615eb5c6a402433fc00fa4bcfdebeacfeb4a342b75723144725498b8652208dba29e4ec52f1ccf232b7544ca4b78bbba6704c404a8dea294a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Betso::Application.config.session_store :cookie_store, key: '_betso_session'
